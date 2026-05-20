698a5fca1ddf2cde7599899dd17ada2a4e474e2f
Felipe <felipe_santos169@estudante.sesisenai.org.br>
2026-05-19 22:04:09 -0300

    quiz definitivo 100% pro max

--- quiz.js
+++ quiz.js
@@ -4,27 +4,36 @@
      {pergunta: "Qual planeta é conhecido como Planeta Vermelho?", opcoes: ["Vênus", "Marte", "Júpiter", "Saturno"], correta: 1},
      {pergunta: "Quanto é 7 x 8?", opcoes: ["54", "56", "64", "58"], correta: 1},
      {pergunta: "Qual destes é um oceano?", opcoes: ["Amazônia", "Saara", "Andes", "Atlântico"], correta: 3}
-     ];
-     const indiceAleatorio = Math.floor(Math.random() * questoes.length);
-     const questaoAtual = questoes[indiceAleatorio];
+];
+const indiceAleatorio = Math.floor(Math.random() * questoes.length);
+const questaoAtual = questoes[indiceAleatorio];
 
-     document.getElementById('pergunta-texto').textContent = questaoAtual.pergunta;
-     const botoes = document.querySelectorAll('.opcao-btn');
-     const feedback = document.getElementById('feedback');
-    
-     botoes.forEach((btn, index) => {
+document.getElementById('pergunta-texto').textContent = questaoAtual.pergunta;
+const botoes = document.querySelectorAll('.opcao-btn');
+const feedback = document.getElementById('feedback');
+
+botoes.forEach((btn, index) => {
      btn.textContent = questaoAtual.opcoes[index];
     
      btn.addEventListener('click', () => {
-     if (index === questaoAtual.correta) {
-     btn.classList.add('correto');
-     feedback.textContent = "🎉 Resposta Correta!";
-     feedback.style.color = "#4CAF50";
-     } else {
-     btn.classList.add('errado');
-     feedback.textContent = "❌ Resposta Incorreta!";
-     feedback.style.color = "#F44336";
-     }
-  
+        if (index === questaoAtual.correta) {
+        btn.classList.add('correto');
+        feedback.textContent = "🎉 Resposta Correta!";
+        feedback.style.color = "#4CAF50";
+        botoes.forEach((sobraBtn, indexSobra) => {
+          if(indexSobra !== index) {
+            sobraBtn.style.visibility = 'hidden';
+          }
+        })
+        } else {
+        btn.classList.add('errado');
+        feedback.textContent = "❌ Resposta Incorreta!";
+        feedback.style.color = "#F44336";
+        botoes.forEach((sobraBtn, indexSobra) => {
+          if(indexSobra !== index) {
+            sobraBtn.style.visibility = 'hidden';
+          }
+        })
+        }
    });
 })
